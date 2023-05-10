0b00aca86dcf0caf19d3cecddd5d558f9d131c25
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 12:41:05 +0300

    Add value input

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 
 const LongPulling = () => {
     const [messages,setMessages] = useState([])
+    const [value,setValue] = useState('')
   return (
     <div className='center'>
         <div>
@@ -9,7 +10,11 @@ const LongPulling = () => {
                 <input type="text" />
                 <button>Отправить</button>
             </div>
-            <div className="messages"></div>
+            <div className="messages">
+                {messages.map((mess)=>{
+            <div className='message' key={mess.id}>{mess.message}</div>
+                })}
+        </div>
         </div>
       
     </div>
