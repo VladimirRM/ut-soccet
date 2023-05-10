68290cda52c101dbded95357af4008ec4c776663
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 12:30:13 +0300

    done serv

--- server/longpulling.js
+++ server/longpulling.js
@@ -11,12 +11,16 @@ const app = express()
 app.use(cors())
 
 app.get('get message',((req,res)=>{
-
+    emitter.once('newMessage',(message)=>{
+       res.json(message)
+    })
 })
 
 )
 app.post('new message',((req,res)=>{
-      
+      const message = req.body
+      emitter.emit('newMessage',message)
+      res.status(200)
 }))
 
 app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
