87439980955f32da3d51956e0e3984cadd61083b
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 12:37:51 +0300

    Add messages state

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const LongPulling = () => {
+    const [messages,setMessages] = useState([])
   return (
     <div className='center'>
         <div>
@@ -8,6 +9,7 @@ const LongPulling = () => {
                 <input type="text" />
                 <button>Отправить</button>
             </div>
+            <div className="messages"></div>
         </div>
       
     </div>
