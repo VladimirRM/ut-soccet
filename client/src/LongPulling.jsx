08a4e8d8a1393a057ab91dcfc82075cf2b0defa0
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 13:07:56 +0300

    create const subscribe

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -1,19 +1,35 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios'
 
 const LongPulling = () => {
     const [messages,setMessages] = useState([])
     const [value,setValue] = useState('')
 
 
-    const sendMessage = ()=>{
-        
+    const sendMessage = async ()=>{
+      await axios.post('http://localhost:5000/new-messages',{
+        message:value,
+        id:Date.now()
+      })
+    }
+
+    useEffect(()=>{
+
+    },[])
+
+    const subscribe = async ()=>{
+        try{
+
+        }catch(e){
+            console.log(e)
+        }
     }
   return (
     <div className='center'>
         <div>
             <div>
                 <input   value={value} onChange={e =>setValue(e.target.value)}type="text" />
-                <button onClick={sendMassage}>Отправить</button>
+                <button onClick={sendMessage}>Отправить</button>
             </div>
             <div className="messages">
                 {messages.map((mess)=>{
--- server/longpulling.js
+++ server/longpulling.js
@@ -10,14 +10,14 @@ const app = express()
 
 app.use(cors())
 
-app.get('get message',((req,res)=>{
+app.get('/get messages',((req,res)=>{
     emitter.once('newMessage',(message)=>{
        res.json(message)
     })
 })
 
 )
-app.post('new message',((req,res)=>{
+app.post('/new message',((req,res)=>{
       const message = req.body
       emitter.emit('newMessage',message)
       res.status(200)
