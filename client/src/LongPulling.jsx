1f754f997bef6b20c532bf9406f298467f332de4
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 12:32:31 +0300

    work with form

---
+++ client/src/LongPulling.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LongPulling = () => {
+  return (
+    <div className='center'>
+        <div>
+            <div>
+                <input type="text" />
+                <button></button>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default LongPulling
