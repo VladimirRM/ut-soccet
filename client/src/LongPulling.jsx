6345d36028ff6a83699d54b8b0e00f5251d30f5a
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 16:56:11 +0300

    change all code

--- client/src/LongPulling.jsx
+++ client/src/LongPulling.jsx
@@ -1,45 +1,51 @@
-import React,{useEffect, useState} from 'react'
-import axios from 'axios'
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const LongPulling = () => {
-    const [messages,setMessages] = useState([])
-    const [value,setValue] = useState('')
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
 
 
-    const sendMessage = async ()=>{
-      await axios.post('http://localhost:5000/new-messages',{
-        message:value,
-        id:Date.now()
-      })
-    }
+    useEffect(() => {
+        subscribe()
+    }, [])
 
-    useEffect(()=>{
-
-    },[])
-
-    const subscribe = async ()=>{
-        try{
-
-        }catch(e){
-            console.log(e)
+    const subscribe = async () => {
+        try {
+            const {data} = await axios.get('http://localhost:5000/get-messages')
+            setMessages(prev => [data, ...prev])
+            await subscribe()
+        } catch (e) {
+            setTimeout(() => {
+                subscribe()
+            }, 500)
         }
     }
-  return (
-    <div className='center'>
-        <div>
-            <div>
-                <input   value={value} onChange={e =>setValue(e.target.value)}type="text" />
-                <button onClick={sendMessage}>Отправить</button>
-            </div>
-            <div className="messages">
-                {messages.map((mess)=>{
-            <div className='message' key={mess.id}>{mess.message}</div>
-                })}
-        </div>
-        </div>
-      
-    </div>
-  )
-}
 
-export default LongPulling
+    const sendMessage = async () => {
+        await axios.post('http://localhost:5000/new-messages', {
+            message: value,
+            id: Date.now()
+        })
+    }
+
+    return (
+        <div className="center">
+            <div>
+                <div className="form">
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text"/>
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(mess =>
+                        <div className="message" key={mess.id}>
+                            {mess.message}
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LongPulling;
