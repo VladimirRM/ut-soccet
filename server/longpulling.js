9bc52ac748ad1e7e48c54ec7fab6b3a3fbfc8114
Vladimir RM <shvdrummer@gmail.com>
2023-05-10 12:26:06 +0300

    adding EventEmitter

--- server/longpulling.js
+++ server/longpulling.js
@@ -1,5 +1,23 @@
 const  express = require('express')
 const  cors = require('cors')
+const events = require('events')
 
+const emitter =new events.EventEmitter()
 
+const  PORT = 5000
+
+const app = express()
+
+app.use(cors())
+
+app.get('get message',((req,res)=>{
+
+})
+
+)
+app.post('new message',((req,res)=>{
+      
+}))
+
+app.listen(PORT,()=> console.log(`Server started on port ${PORT}`))
 
